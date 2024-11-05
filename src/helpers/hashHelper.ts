9944480ec208f8059a2645eca60fc7af874f88af
Ramon Santos <117384857+ramonsantz@users.noreply.github.com>
2024-11-05 20:16:36 -0300

    Update hashHelper.ts

--- src/helpers/hashHelper.ts
+++ src/helpers/hashHelper.ts
@@ -1,13 +1,14 @@
-// src/helpers/hashHelper.ts
 import crypto from 'crypto';
 
 export const hashPassword = (password: string): string => {
-    const hash = crypto.createHash('sha256'); // Você pode escolher o algoritmo que preferir
+    const hash = crypto.createHash('sha256'); 
     hash.update(password);
+    
     return hash.digest('hex');
 };
 
 export const comparePassword = (password: string, hash: string): boolean => {
     const hashedPassword = hashPassword(password);
+    
     return hashedPassword === hash;
 };
