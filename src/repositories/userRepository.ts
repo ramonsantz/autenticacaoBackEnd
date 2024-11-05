866d6aff313fbc63fbd9fc1d2cbc0d0de8f7e94b
Ramon Santos <117384857+ramonsantz@users.noreply.github.com>
2024-11-05 20:13:00 -0300

    Update userRepository.ts

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -7,16 +7,20 @@ export class UserRepository {
 
   constructor() {
     this.pool = pool;
+    
   }
 
   async getUserByEmail(email: string): Promise<User | null> {
     const { rows } = await this.pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    
     return rows[0] || null;
   }
 
   async addUser(name: string, email: string, passwordHash: string): Promise<User> {
+    
     const queryText = 'INSERT INTO users(name, email, passwordHash) VALUES($1, $2, $3) RETURNING *';
     const { rows } = await this.pool.query(queryText, [name, email, passwordHash]);
+    
     return rows[0];
   }
 }
