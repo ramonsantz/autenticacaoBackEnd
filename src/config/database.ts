620c693cd14389780df9c63d3f0b998cbe70b5ad
Ramon Santos <117384857+ramonsantz@users.noreply.github.com>
2024-11-05 20:17:10 -0300

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,5 @@
 import { Pool } from 'pg';
 
-// Nova string de conexão do Render.com
 const connectionString = 'postgresql://autenticacaousuariofecaf_user:zpMBhSjom5X195cIlIVcXjfi5bdaMrlW@dpg-csglh53qf0us739n5s8g-a.oregon-postgres.render.com/autenticacaousuariofecaf';
 
 const pool = new Pool({
@@ -8,6 +7,7 @@ const pool = new Pool({
   ssl: {
     rejectUnauthorized: false, 
   },
+  
 });
 
 export default pool;
